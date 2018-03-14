307fa3e696596a748245e7ee2c7517a487df1091
cannotdescribe <952376923@qq.com>
2018-03-15 02:08:45 +0800

    jhj

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
-module.exports = {
+const isDev = process.env.NODE_ENV === "development"; // 启动时设置的全局变量，会有都在process.env当中。
+
+var config = {
+    target: "web",
     entry: path.join(__dirname, "src/index.js"),
     output: {
         filename: "bundle.js",
@@ -29,16 +34,37 @@ module.exports = {
             },
             {
                 test: /\.(gif|jpg|jpeg|png|svg)$/,
-                use: [
-                    {
-                        loader: "url-loader",
-                        options: {
-                            limit: 1024,   // 小于1024 就会将图片转换为base64代码，写入到输出文件中
-                            name: '[name].[ext]'
-                        }
-                    }
-                ]
+                // use: [
+                //     {
+                //         loader: "url-loader",
+                //         options: {
+                //             limit: 8192,   // 小于1024 就会将图片转换为base64代码，写入到输出文件中
+                //             name: '[name].[ext]'
+                //         }
+                //     }
+                // ]
+                loader: 'url-loader?limit=100000'
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
+        new HTMLPlugin()
+    ]
 };
+
+if(isDev){
+    config.devServer = {
+        port: '8000',
+        host: "0.0.0.0",
+        overlay: {
+            errors: true //让错误直接显示到网页上
+        }
+    }
+}
+
+module.exprots = config;
